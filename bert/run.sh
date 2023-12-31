c84282a0787b37433eb86e32a5cbac63507841f6
4LDK <chenran@hotmail.co.jp>
2023-12-31 23:21:51 +0900

    fix sh

--- bert/run.sh
+++ bert/run.sh
@@ -8,12 +8,12 @@ export PATH=~/.local/bin:$PATH
 
 model_name="bert-base-cased"
 
-sed -z "s/\nmodel_name: /\nmodel_name:$model_name  #/" ./config/conll2003.yaml > ./config/conll2003_modeled.yaml
-python3 ./bert/run.py --config conll2003_modeled
+sed -z "s/\nmodel_name: /\nmodel_name:$model_name  #/" ./config/conll2003.yaml > ./config/conll2003_bert.yaml
+python3 ./bert/run.py --config conll2003_bert
 
-sed -z "s/\np: 0/\np: 0.1/" ./config/conll2003_modeled.yaml > ./config/conll2003_01.yaml
-python3 ./bert/run.py --config conll2003_01
+sed -z "s/\np: 0/\np: 0.1/" ./config/conll2003_bert.yaml > ./config/conll2003_bert01.yaml
+python3 ./bert/run.py --config conll2003_bert01
 
-sed -z "s/\np: 0/\np: 0.3/" ./config/conll2003_modeled.yaml > ./config/conll2003_03.yaml
-python3 ./bert/run.py --config conll2003_03
+sed -z "s/\np: 0/\np: 0.3/" ./config/conll2003_bert.yaml > ./config/conll2003_bert03.yaml
+python3 ./bert/run.py --config conll2003_bert03
   
--- roberta/run.sh
+++ roberta/run.sh
@@ -8,12 +8,12 @@ export PATH=~/.local/bin:$PATH
 
 model_name="roberta-large"
 
-sed -z "s/\nmodel_name: /\nmodel_name:$model_name  #/" ./config/conll2003.yaml > ./config/conll2003_modeled.yaml
-sed -i -z "/\nlength: /\nlength: 510 #/" ./config/conll2003_modeled.yaml
-python3 ./roberta/run.py --config conll2003_modeled
+sed -z "s/\nmodel_name: /\nmodel_name:$model_name  #/" ./config/conll2003.yaml > ./config/conll2003_roberta.yaml
+sed -i -z "/\nlength: /\nlength: 510 #/" ./config/conll2003_roberta.yaml
+python3 ./roberta/run.py --config conll2003_roberta
 
-sed -z "s/\np: 0/\np: 0.1/" ./config/conll2003_modeled.yaml > ./config/conll2003_01.yaml
-python3 ./roberta/run.py --config conll2003_01
+sed -z "s/\np: 0/\np: 0.1/" ./config/conll2003_roberta.yaml > ./config/conll2003_roberta01.yaml
+python3 ./roberta/run.py --config conll2003_roberta01
 
-sed -z "s/\np: 0/\np: 0.3/" ./config/conll2003_modeled.yaml > ./config/conll2003_03.yaml
-python3 ./roberta/run.py --config conll2003_03
+sed -z "s/\np: 0/\np: 0.3/" ./config/conll2003_roberta.yaml > ./config/conll2003_roberta03.yaml
+python3 ./roberta/run.py --config conll2003_roberta03
